fe2c8acf79f354e6f38b776e60b217dd36bda012
hello3rd <hello3rd@outlook.com>
2023-11-18 22:45:04 +0800

    chore: 适配至ESP-IDF v5.1.2，LVGL v8.3.10
    - 引入hiruna的适配后的`lvgl_esp32_drivers`
    - `menuconfig`增加st7735s的x、y轴偏移值配置
    - 移植到合宙ESP32C3-CORE开发板和0.96寸屏幕拓展板，实现开箱即用
    - 改为使用 IDF 组件管理器管理组件

--- main/main.c
+++ main/main.c
@@ -1,216 +1,167 @@
-/* LVGL Example project
+#include <sys/cdefs.h>
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
  *
- * Basic project to test LVGL on ESP32 based projects.
- *
- * This example code is in the Public Domain (or CC0 licensed, at your option.)
- *
- * Unless required by applicable law or agreed to in writing, this
- * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
+ * SPDX-License-Identifier: CC0-1.0
  */
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_timer.h"
+#include "esp_freertos_hooks.h"
+#include "esp_log.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_freertos_hooks.h"
-#include "freertos/semphr.h"
-#include "esp_system.h"
-#include "esp_timer.h"
-#include "driver/gpio.h"
-
-/* Littlevgl specific */
-#ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
-#else
-#include "lvgl/lvgl.h"
-#endif
-
 #include "lvgl_helpers.h"
 
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
-    #else
-        #error "No demo application selected."
-    #endif
-#endif
+#define LED_4 12
+#define LED_5 13
+#define LOW_LEVEL 0
+#define HIGH_LEVEL 1
 
-/*********************
- *      DEFINES
- *********************/
-#define TAG "demo"
-#define LV_TICK_PERIOD_MS 1
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-static void lv_tick_task(void *arg);
-static void guiTask(void *pvParameter);
-static void create_demo_application(void);
-
-/**********************
- *   APPLICATION MAIN
- **********************/
-void app_main() {
-
-    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
-     * Otherwise there can be problem such as memory corruption and so on.
-     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
-    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+void lv_tick_task(void *arg) {
+  (void) arg;
+  lv_tick_inc(1);
 }
 
-/* Creates a semaphore to handle concurrent call to lvgl stuff
- * If you wish to call *any* lvgl function from other threads/tasks
- * you should lock on the very same semaphore! */
-SemaphoreHandle_t xGuiSemaphore;
+_Noreturn void PrintChipInfo(void *params) {
+  (void) params;
+  /* Print chip information */
+  esp_chip_info_t chip_info;
+  uint32_t flash_size;
+  esp_chip_info(&chip_info);
+  printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+         CONFIG_IDF_TARGET,
+         chip_info.cores,
+         (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+         (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+         (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+         (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
 
-static void guiTask(void *pvParameter) {
+  unsigned major_rev = chip_info.revision / 100;
+  unsigned minor_rev = chip_info.revision % 100;
+  printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+  if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+    printf("Get flash size failed");
+  }
 
-    (void) pvParameter;
-    xGuiSemaphore = xSemaphoreCreateMutex();
+  printf("%"
+         PRIu32
+         "MB %s flash\n", flash_size / (uint32_t) (1024 * 1024),
+         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    lv_init();
+  printf("Minimum free heap size: %"
+         PRIu32
+         " bytes\n", esp_get_minimum_free_heap_size());
 
-    /* Initialize SPI or I2C bus used by the drivers */
-    lvgl_driver_init();
+  while (true) {
+    // 任务禁止主动返回
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
-    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
-    assert(buf1 != NULL);
+_Noreturn void BlinkLed(void *params) {
+  (void) params;
+  uint8_t level = LOW_LEVEL;
+  gpio_reset_pin(LED_4);
+  gpio_set_direction(LED_4, GPIO_MODE_OUTPUT); // Set the GPIO as a push/pull output
+  gpio_reset_pin(LED_5);
+  gpio_set_direction(LED_5, GPIO_MODE_OUTPUT); // Set the GPIO as a push/pull output
+  ESP_LOGI("BlinkLed", "LED configuration completed.");
 
-    /* Use double buffered when not working with monochrome displays */
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
-    assert(buf2 != NULL);
-#else
-    static lv_color_t *buf2 = NULL;
-#endif
+  while (true) {
+    gpio_set_level(LED_4, level);
+    ESP_LOGI("BlinkLed", "LED_4: %s!", level == HIGH_LEVEL ? "ON" : "OFF");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-    static lv_disp_buf_t disp_buf;
+    level = !level;
 
-    uint32_t size_in_px = DISP_BUF_SIZE;
+    gpio_set_level(LED_5, level);
+    ESP_LOGI("BlinkLed", "LED_5: %s!", level == HIGH_LEVEL ? "ON" : "OFF");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
-#if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820         \
-    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A    \
-    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D     \
-    || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
+_Noreturn void app_main(void) {
 
-    /* Actual size in pixels, not bytes. */
-    size_in_px *= 8;
-#endif
+  xTaskCreate(PrintChipInfo, "PrintChipInfo", 1024 * 4, NULL, 1, NULL);
+  xTaskCreate(BlinkLed, "BlinkLed", 1024 * 4, NULL, 1, NULL);
 
-    /* Initialize the working buffer depending on the selected display.
-     * NOTE: buf2 == NULL when using monochrome displays. */
-    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+  fflush(stdout);
 
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.flush_cb = disp_driver_flush;
-
-#if defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT || defined CONFIG_DISPLAY_ORIENTATION_PORTRAIT_INVERTED
-    disp_drv.rotated = 1;
-#endif
-
-    /* When using a monochrome display we need to register the callbacks:
-     * - rounder_cb
-     * - set_px_cb */
-#ifdef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    disp_drv.rounder_cb = disp_driver_rounder;
-    disp_drv.set_px_cb = disp_driver_set_px;
-#endif
-
-    disp_drv.buffer = &disp_buf;
-    lv_disp_drv_register(&disp_drv);
-
-    /* Register an input device when enabled on the menuconfig */
-#if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.read_cb = touch_driver_read;
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    lv_indev_drv_register(&indev_drv);
-#endif
-
-    /* Create and start a periodic timer interrupt to call lv_tick_inc */
-    const esp_timer_create_args_t periodic_timer_args = {
-        .callback = &lv_tick_task,
-        .name = "periodic_gui"
-    };
-    esp_timer_handle_t periodic_timer;
-    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
-
-    /* Create the demo application */
-    create_demo_application();
-
-    while (1) {
-        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
-        vTaskDelay(pdMS_TO_TICKS(10));
-
-        /* Try to take the semaphore, call lvgl related function on success */
-        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
-            lv_task_handler();
-            xSemaphoreGive(xGuiSemaphore);
-       }
+  {
+    TaskHandle_t print_chip_info_handle = xTaskGetHandle("PrintChipInfo");
+    if (print_chip_info_handle != NULL) {
+      vTaskDelete(print_chip_info_handle);
+      ESP_LOGI("app_main", "Task PrintChipInfo delete.");
     }
+  }
 
-    /* A task should NEVER return */
-    free(buf1);
+  /**
+   * \brief Start LVGL demo.
+   */
+  lv_init();
+  lvgl_driver_init();
+  lv_color_t *buf1 =
+      heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+  assert(buf1 != NULL);
+  /* Use double buffered when not working with monochrome displays */
 #ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    free(buf2);
-#endif
-    vTaskDelete(NULL);
-}
-
-static void create_demo_application(void)
-{
-    /* When using a monochrome display we only show "Hello World" centered on the
-     * screen */
-#if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
-    defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
-
-    /* use a pretty small demo for monochrome displays */
-    /* Get the current screen  */
-    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
-
-    /*Create a Label on the currently active screen*/
-    lv_obj_t * label1 =  lv_label_create(scr, NULL);
-
-    /*Modify the Label's text*/
-    lv_label_set_text(label1, "Hello\nworld");
-
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
-    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
+  lv_color_t *buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+  assert(buf2 != NULL);
 #else
-    /* Otherwise we show the selected demo */
-
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        lv_demo_widgets();
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        lv_demo_keypad_encoder();
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        lv_demo_benchmark();
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        lv_demo_stress();
-    #else
-        #error "No demo application selected."
-    #endif
+  static lv_color_t *buf2 = NULL;
 #endif
-}
+  static lv_disp_draw_buf_t disp_buf;
+  uint32_t size_in_px = DISP_BUF_SIZE;
+  lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
+  lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = CONFIG_LV_HOR_RES_MAX;
+  disp_drv.ver_res = CONFIG_LV_VER_RES_MAX;
+  disp_drv.flush_cb = disp_driver_flush;
+  disp_drv.draw_buf = &disp_buf;
+  lv_disp_drv_register(&disp_drv);
 
-static void lv_tick_task(void *arg) {
-    (void) arg;
+  /* Register an input device when enabled on the menuconfig */
+#if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
+  ESP_LOGI(TAG, "TOUCH CONTROLLER");
+  lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.read_cb = touch_driver_read;
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  lv_indev_drv_register(&indev_drv);
+#endif
 
+  const esp_timer_create_args_t periodic_timer_args = {
+      .callback = &lv_tick_task, .name = "screen"};
+  esp_timer_handle_t periodic_timer;
+  ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+  ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 1000));
 
-    lv_tick_inc(LV_TICK_PERIOD_MS);
+  ESP_LOGI(__FILENAME__, "Free Heap Size: %lu", esp_get_minimum_free_heap_size());
+
+  lv_obj_t *label = lv_label_create(lv_scr_act());
+  if (NULL != label) {
+    lv_label_set_text(label, "Hello world");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  }
+
+  while (1) {
+    vTaskDelay(pdMS_TO_TICKS(10));
+    lv_task_handler();
+  }
+
+  free(buf1);
+  free(buf2);
+  vTaskDelete(NULL);
 }
